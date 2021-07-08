f8f66e4b4165f322dc4ef8a65bb6f56fc667db66
argun8394 <argundoganali@gmail.com>
2021-07-08 23:56:22 +0300

    DOM

---
+++ 08_07_DOM/script.js
@@ -0,0 +1,30 @@
+// JS DOM
+
+// document.getElementBy...
+// const myDom = document.getElementById('dom');
+
+// console.log(myDom);
+
+// const instructor = document.getElementById('info');
+// console.log(instructor);
+
+// const ins = document.getElementById('instructor');
+// console.log(ins);
+
+// Classname
+
+// const withClassName = document.getElementsByClassName('class-info');
+// console.log(withClassName[0]);
+
+// TagName
+// const cw = document.getElementsByTagName('h3');
+// console.log(cw);
+
+// document.querySelector
+
+// console.log(document.querySelectorAll('.class-info'));
+
+// console.log(document.querySelector('#instructor'));
+
+// console.log(document.querySelectorAll('h3')[1]);
+console.log(document.querySelector(".class-info h3"));
