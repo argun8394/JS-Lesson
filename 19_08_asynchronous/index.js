7cae8fb764d68e60e850a750ff884e48cdeca5ff
argun8394 <argundoganali@gmail.com>
2021-08-19 21:15:22 +0300

    setTimeout

--- 19_08_asynchronous/index.js
+++ 19_08_asynchronous/index.js
@@ -1,8 +1,23 @@
 //setTimeout
+const slowTask = () => {
+    let i = 0;
+    do {
+        i++;
+    } while (i < 500000000);
+    return "task is completed.";
+};
 
+console.log("¯\\_(ツ)_/¯");
+console.time("bir");
+// setTimeout
+console.log("hello1");
+console.timeLog("bir");
+slowTask();
 setTimeout(() => {
-    console.log("Hello from settimeout");
-
-}, 2000);
+    console.log("Settimeout");
+    console.timeEnd("bir");
+}, 1000);
 console.log("hello2");
+console.timeLog("bir");
 console.log("hello3");
+console.timeLog("bir");
